cb50fa28d2c2f36656b3be47108e12013dcda53a
AkaakuHub <105225777+github-actions@users.noreply.github.com>
2024-12-11 14:38:09 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,69 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import { Button, Grid2, TextField } from "@mui/material";
+import type React from "react";
+import { useState } from "react";
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function Page() {
+  const [userInput, setUserInput] = useState("");
+  const [resultText, setResultText] = useState("");
+
+  const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+    setUserInput(input);
+    const result = `${input.split("").join("゛")}゛`;
+    setResultText(result);
+  }
+
+  const copyHandler = () => {
+    try {
+      navigator.clipboard.writeText(resultText);
+      toast.success("コピーしました！");
+    } catch (error) {
+      console.error(error);
+      toast.error("コピーに失敗しました");
+    }
+  }
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <Grid2
+        container
+        spacing={2}
+        direction={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        sx={{ height: "100dvh" }}
+      >
+        <TextField value={userInput} onChange={inputHandler} sx={{
+          width: "80%",
+        }} id="filled-basic" label="ここに入力" variant="filled"
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <ArrowDownwardIcon />
+        <TextField value={resultText} sx={{
+          width: "80%",
+        }}
+          id="outlined-basic" label="結果" variant="outlined"
+          inputProps={{ readOnly: true }}
+        />
+        <Button variant="contained" onClick={() => { copyHandler() }}>
+          コピーする
+        </Button>
+      </Grid2>
+    </>
   );
 }
